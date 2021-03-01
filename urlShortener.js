8e39c1a61bfe9028c0adb1b93ee3aa1269c32ab5
Alex Bobb <ab7253@nyu.edu>
2021-03-01 14:31:26 -0800

    Fixed url shortener

--- urlShortener.js
+++ urlShortener.js
@@ -1,5 +1,7 @@
 
 const Shortener = require("@studiohyperdrive/shortener");
+//add node module
+var url = require('url');
 
 class URLShortener {
     constructor (originalURL) {
@@ -10,10 +12,11 @@ class URLShortener {
 	// Returns Short URL
     shorten() {
         // TODO parse out protocol from this.originalURL, then use that as the target below
-        let protocol = 'http';
+        const myURL = new URL(this.originalURL);
+        let protocol = myURL.protocol;
 
         const shortener = new Shortener({
-            target: `${protocol}://localhost:3000`,
+            target: `${protocol}//localhost:3000`,
             length: 6,
             alphabet: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         });
@@ -23,6 +26,8 @@ class URLShortener {
 
 	// Returns Expanded URL
     expand() {
+        const myURL = new URL(this.originalURL);
+        let protocol = myURL.protocol;
     }
 
 	// Updates Click count
